08d2c1a55924e5f0e03d341ef40c93435419b5f4
Tony Englese <tenglese@gmail.com>
2015-10-13 22:07:06 -0700

    Initial Commit

--- GridTest/DataViewController.swift
+++ GridTest/DataViewController.swift
@@ -17,6 +17,7 @@ class DataViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("I can't wait to push this project to GitHub")
     }
 
     override func didReceiveMemoryWarning() {
