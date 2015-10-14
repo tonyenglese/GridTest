fec0cd5680a19a169397262467728107102eab17
Tony Englese <tenglese@gmail.com>
2015-10-13 22:01:45 -0700

    Initial Commit

---
+++ GridTest/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  GridTest
+//
+//  Created by Tony Englese on 10/13/15.
+//  Copyright © 2015 ETESTDEV. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
